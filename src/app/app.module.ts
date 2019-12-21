b263ac859a3791d8239cf7e18515b59fe856eea5
Akhilesh Chaurasiya <akhilesh.chaurasiya@gmail.com>
2019-12-21 16:44:29 +0530

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ParticipationComponent } from './participations/participation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ParticipationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/participations/participation.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:"participation-component",
+    templateUrl: './participation.component.html'
+})
+export class ParticipationComponent{
+    name="Akhilesh";
+}
